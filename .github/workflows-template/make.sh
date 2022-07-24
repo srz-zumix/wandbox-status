25bd8a5b4830b38fd2318e0cf3a3cca053aa83ec
srz_zumix <zumix.cpp@gmail.com>
2022-07-24 13:22:43 +0900

    update workflows

--- .github/workflows-template/make.sh
+++ .github/workflows-template/make.sh
@@ -2,10 +2,12 @@
 
 SELF_DIR=$(cd $(dirname $0); pwd)
 WANDBOX_OPTIONS=
+FORCE_UPDATE=false
 
-while getopts h OPT
+while getopts fh OPT
 do
     case $OPT in
+        f) FORCE_UPDATE=true ;;
         h) usage_exit ;;
         *) usage_exit ;;
     esac
@@ -16,11 +18,27 @@ MIN=0
 
 while IFS= read -a line ; do {
     LANG=${line}
+    echo ${LANG}
+    TARGET_FILE="${SELF_DIR}/../workflows/${LANG}.yml"
+    if "${FORCE_UPDATE}"; then
+        if [ -f "${TARGET_FILE}" ]; then
+            rm "${TARGET_FILE}"
+        fi
+    fi
     HOUR=`echo "($HOUR + 1) % 12" | bc`
     HOUR2=`echo "($HOUR + 12) % 24" | bc`
-    MIN=`echo "($MIN + 18) % 60" | bc`
-    echo ${LANG}
-    sed -e "s/TEMPLATE_LANGUAGE/${LANG}/g" -e "s/TEMPLATE_HOUR/${HOUR},${HOUR2}/g" -e "s/TEMPLATE_MIN/${MIN}/g" "${SELF_DIR}/docs-template.yml" > "${SELF_DIR}/../workflows/${LANG}.yml"
+    HOURS="${HOUR},${HOUR2}"
+    if [ -f "${TARGET_FILE}" ]; then
+        MIN_HOUR=$(cat "${TARGET_FILE}" | grep -o "cron:.*" | head -1 | grep -oE "[0-9,-]+\s[0-9,-]+")
+        # echo "${MIN_HOUR}"
+        MINS="${MIN_HOUR% *}"
+        HOURS="${MIN_HOUR#* }"
+        # HOUR="${HOURS%,*}"
+    else
+        MIN=`echo "($MIN + 18) % 60" | bc`
+        MINS="${MIN}"
+    fi
+    sed -e "s/TEMPLATE_LANGUAGE/${LANG}/g" -e "s/TEMPLATE_HOUR/${HOURS}/g" -e "s/TEMPLATE_MIN/${MINS}/g" "${SELF_DIR}/docs-template.yml" > "${TARGET_FILE}"
 };
 done < <(wandbox ${WANDBOX_OPTIONS} -V lang)
 unset line;
